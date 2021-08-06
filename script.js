eff6d55f45cefb142980b878f1c48059e1aecb9e
James Broski <james.broski.5@gmail.com>
2021-08-06 16:48:08 -0600

    Troubleshooting functionality

--- script.js
+++ script.js
@@ -1,43 +1,38 @@
-var currentMoment = moment().format("dddd, MMM Do YYYY");
-$("#currentDay").val(currentMoment);
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour13 = $("#13");
+var hour14 = $("#14");
+var hour15 = $("#15");
+var hour16 = $("#16");
+var hour17 = $("#17");
 
-$(dayPlanner).ready(function () {
-  function timeOfDay() {
-    var thisMoment = moment().hour();
-    $(".input-group").each(function () {
-      var thatMoment = parseInt($(this).attr("id").split("hour")[1]);
+var savebtn = $(".saveBtn");
+var date = $("#currentDay");
+var today = moment();
 
-      if (thatMoment < thisMoment) {
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-        $(this).addClass("past");
-      } else if (thatMoment === thisMoment) {
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
-      } else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-      }
-    });
+function setDate() {
+  $("#currentDay").text(today.format("dddd MMM Do, YYYY"));
+  UpdateTime();
+}
+function UpdateTime() {
+  var currentTime = moment().format("H");
+  for (var i = 9; i <= 17; i++) {
+    if (i > currentTime) {
+      $("#" + i).addClass(".future");
+    } else if (i < currentTime) {
+      $("#" + i).addClass(".past");
+    } else if (i == currentTime) {
+      $("#" + i).addClass(".present");
+    }
+    $("#t" + i).val(localStorage.getItem("s" + i));
   }
+}
+setDate();
 
-  $(".saveEvent").on("click", function () {
-    var saveText = $(this).val();
-    var saveTime = $(this).val();
-    localStorage.setItem(saveText, saveTime);
-  });
-
-  $("#event9am .description").val(localStorage.getItem("event9am"));
-  $("#event10am .description").val(localStorage.getItem("event10am"));
-  $("#event11am .description").val(localStorage.getItem("event11am"));
-  $("#event12pm .description").val(localStorage.getItem("event12pm"));
-  $("#event1pm .description").val(localStorage.getItem("event1pm"));
-  $("#event2pm .description").val(localStorage.getItem("event2pm"));
-  $("#event3pm .description").val(localStorage.getItem("event3pm"));
-  $("#event4pm .description").val(localStorage.getItem("event4pm"));
-  $("#event5pm .description").val(localStorage.getItem("event5pm"));
-
-  timeOfDay();
+savebtn.on("click", function () {
+  var text = $(this).siblings("textarea").val();
+  var btnid = $(this).attr("id");
+  localStorage.setItem(btnid, text);
 });
